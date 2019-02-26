25a9c3dcd2eda9f4fa38823766a455be9a040d88
Boris Balaban <BorisBalaban71@gmail.com>
2019-02-26 09:09:26 +0300

    Create player

--- Assets/Script/AddComponent.cs
+++ Assets/Script/AddComponent.cs
@@ -8,52 +8,26 @@ public class AddComponent : MonoBehaviour
     public GameObject block;
     private GameObject inst_block;
 
-    private int width = 9;
-    private int height = 11;
+    private int width = 7;
+    private int height = 7;
 
     public GameObject obstacle;
     private GameObject inst_obstacle;
     private System.Random rand = new System.Random();
 
+    public GameObject player;
+    private GameObject inst_player;
+    int playerX = 0;
+    int playerY = 0;
     void Start()
     {
-        CreateBlocks();
         CreateWalls();
+        CreatePlayers();
+        CreateBlocks(playerX, playerY);
     }
 
     void Update()
     {
-        PutBomb();
-    }
-
-    void CreateObstacles(int x, int y)
-    {
-        int k = rand.Next(10);
-
-        if (k > 7)
-        {
-            GameObject inst_obstacle = Instantiate(obstacle, new Vector3(x, 0.0f, y), Quaternion.identity) as GameObject;
-            Renderer rend = inst_obstacle.GetComponent<Renderer>();
-            rend.material.SetColor("_Color", Color.green);
-        }
-    }
-
-    void CreateBlocks()
-    {
-        for (int i = 0; i < width; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
-                if (i % 2 == 0 && j % 2 == 0)
-                {
-                    inst_block = Instantiate(block, new Vector3(0.0f + i, 0.0f, 0.0f + j), Quaternion.identity) as GameObject;
-                }
-                else
-                {
-                    CreateObstacles(i, j);
-                }
-            }
-        }
     }
 
     void CreateWalls()
@@ -64,15 +38,64 @@ public class AddComponent : MonoBehaviour
             {
                 if (i == -2 || i == width + 1 || j == -2 || j == height + 1)
                 {
-                    inst_block = Instantiate(block, new Vector3(0.0f + i, 0.0f, 0.0f + j), Quaternion.identity) as GameObject;
+                    inst_block = Instantiate(block, new Vector3(0.0f + i, 0.5f, 0.0f + j), Quaternion.identity) as GameObject;
                 }
             }
         }
     }
 
-    void PutBomb()
+    void CreateObstacles(int x, int y)
     {
-        if (Input.GetMouseButtonDown(0))
-            Debug.Log("Pressed left click.");
+        int k = rand.Next(10);
+
+        if (k > 7)
+        {
+            GameObject inst_obstacle = Instantiate(obstacle, new Vector3(x, 0.5f, y), Quaternion.identity) as GameObject;
+            Renderer rend = inst_obstacle.GetComponent<Renderer>();
+            rend.material.SetColor("_Color", Color.green);
+        }
+    }
+
+    void CreateBlocks(int ifPlayerX, int ifPlayerY)
+    {
+        int count = 0;
+        for (int i = -1; i <= width; i++)
+        {
+            for (int j = -1; j <= height; j++)
+            {
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    inst_block = Instantiate(block, new Vector3(0.0f + i, 0.5f, 0.0f + j), Quaternion.identity) as GameObject;
+                }
+                else if (i != ifPlayerX || j != ifPlayerY)
+                {
+                    CreateObstacles(i, j);
+                    if ((i == ifPlayerX + 1 && j == ifPlayerY) ||
+                        (i == ifPlayerX && j == ifPlayerY + 1) ||
+                        (i == ifPlayerX - 1 && j == ifPlayerY) ||
+                        (i == ifPlayerX && j == ifPlayerY - 1))
+                    {
+                        count++;
+                        if (count == 2)
+                        {
+                            Destroy(inst_obstacle);
+                            count--;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public (int playerX, int playerY) CreatePlayers()
+    {
+        while (playerX % 2 == 0 || playerY % 2 == 0)
+        {
+            playerX = rand.Next(-1, width + 1);
+            playerY = rand.Next(-1, height + 1);
+        }
+        inst_player = Instantiate(player, new Vector3(playerX, 0.0f, playerY), Quaternion.identity) as GameObject;
+
+        return (playerX, playerY);
     }
 }
